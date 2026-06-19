3e9b9db199e9f43a36c283d20379a6f08d301c4b
Mahadi <mahadibhuyan6@gmail.com>
2026-06-19 11:01:55 +0600

    annotations_to_void

---
+++ annotations.ts
@@ -0,0 +1,25 @@
+//sample
+
+type person={
+    name: string;
+};
+
+const pk:person={
+    name:"john"
+}
+
+console.log(pk);
+
+//numbers string any
+
+let num : number=9;
+console.log(num);
+
+let name:string="Mahadi"
+
+console.log(name);
+
+//boolean
+
+let t_f:boolean=true;
+console.log(t_f);
---
+++ functions.ts
@@ -0,0 +1,6 @@
+function add(num:number){
+    return num+1;
+}
+
+let number=add(8);
+console.log(number);
